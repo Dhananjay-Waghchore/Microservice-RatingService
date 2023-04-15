0856a1d5b1d3d83f26dbcaffc3210404d9dd64a3
Dhananjay-Waghchore <dhananjaywaghchore95@gmail.com>
2023-04-15 13:05:17 +0530

    Initial Commit

---
+++ Microservices-RatingService/src/main/java/com/microservices/ratingservice/MicroservicesRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesRatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesRatingServiceApplication.class, args);
+	}
+
+}
